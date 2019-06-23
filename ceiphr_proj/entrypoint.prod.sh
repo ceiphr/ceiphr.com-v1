9595189a475a1065e58f34e2002d7c0645166089
ceiphr <21004034+ceiphr@users.noreply.github.com>
2019-06-23 11:38:05 -0400

    Fixed entrypoint scripts

--- ceiphr_proj/entrypoint.prod.sh
+++ ceiphr_proj/entrypoint.prod.sh
@@ -11,9 +11,9 @@ then
     echo "PostgreSQL started"
 fi
 
-python3 /usr/src/app/ceiphr_proj/manage.py runserver 0.0.0.0:8000
-python3 /usr/src/app/ceiphr_proj/manage.py makemigrations
-python3 /usr/src/app/ceiphr_proj/manage.py migrate
-python3 /usr/src/app/ceiphr_proj/manage.py collectstatic --noinput --clear
+python3 /usr/src/app/manage.py runserver 0.0.0.0:8000
+python3 /usr/src/app/manage.py makemigrations
+python3 /usr/src/app/manage.py migrate
+python3 /usr/src/app/manage.py collectstatic --noinput --clear
 
 exec "$@"
--- ceiphr_proj/entrypoint.sh
+++ ceiphr_proj/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 /usr/src/app/ceiphr_proj/manage.py runserver 0.0.0.0:8000
-python3 /usr/src/app/ceiphr_proj/manage.py makemigrations
-python3 /usr/src/app/ceiphr_proj/manage.py migrate
-python3 /usr/src/app/ceiphr_proj/manage.py collectstatic --noinput --clear
+python3 /usr/src/app/manage.py runserver 0.0.0.0:8000
+python3 /usr/src/app/manage.py makemigrations
+python3 /usr/src/app/manage.py migrate
+python3 /usr/src/app/manage.py collectstatic --noinput --clear
