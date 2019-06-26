9f88b4da81dcf8d6fcf3ba6260c05ea4f04ad6d4
ceiphr <21004034+ceiphr@users.noreply.github.com>
2019-06-26 19:51:53 -0400

    Fix

--- ceiphr_proj/ceiphr/settings/production.py
+++ ceiphr_proj/ceiphr/settings/production.py
@@ -33,12 +33,13 @@ AWS_S3_CUSTOM_DOMAIN = 'cdn.ceiphr.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_QUERYSTRING_AUTH = True
 AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = ''
 
 STATIC_ROOT = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 
-COMPRESS_URL = STATIC_URL = 'https://cdn.ceiphr.com/%s/' % AWS_LOCATION
+COMPRESS_URL = STATIC_URL = 'https://cdn.ceiphr.com/' % AWS_LOCATION
 COMPRESS_OUTPUT_DIR = ''
 
 AWS_PUBLIC_MEDIA_LOCATION = '%s/media/public' % AWS_LOCATION
