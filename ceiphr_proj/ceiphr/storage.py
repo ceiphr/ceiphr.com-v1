8caa970e9a755a3b731bd1a87230663ceddcd3ea
ceiphr <21004034+ceiphr@users.noreply.github.com>
2019-06-26 15:36:23 -0400

    Testing

--- ceiphr_proj/ceiphr/storage.py
+++ ceiphr_proj/ceiphr/storage.py
@@ -2,5 +2,6 @@ from django.contrib.staticfiles.storage import CachedFilesMixin, ManifestFilesMi
 from pipeline.storage import PipelineMixin
 from storages.backends.s3boto import S3BotoStorage
 
-class S3PipelineManifestStorage(PipelineMixin, ManifestFilesMixin, S3BotoStorage):
-    location = settings.STATICFILES_LOCATION
+
+class S3PipelineManifestStorage(PipelineMixin, CachedFilesMixin, S3BotoStorage):
+    pass
