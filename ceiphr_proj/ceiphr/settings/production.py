9ec5de0a049134877af42d054e79de279ada8e02
ceiphr <21004034+ceiphr@users.noreply.github.com>
2019-06-26 15:43:34 -0400

    Testing stuff

--- ceiphr_proj/ceiphr/settings/production.py
+++ ceiphr_proj/ceiphr/settings/production.py
@@ -29,12 +29,12 @@ AWS_ACCESS_KEY_ID = os.environ.get('SPACES_ACCESS_KEY', 'spaces-access-key')
 AWS_SECRET_ACCESS_KEY = os.environ.get('SPACES_SECRET_ACCESS_KEY', 'spaces-secret-access-key')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('STORAGE_BUCKET_NAME', 'storage-bucket-name')
 AWS_S3_ENDPOINT_URL = 'https://sfo2.digitaloceanspaces.com'
-AWS_S3_CUSTOM_DOMAIN = 'https://cdn.ceiphr.com'
+# AWS_S3_CUSTOM_DOMAIN = 'https://cdn.ceiphr.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = ''
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 AWS_PUBLIC_MEDIA_LOCATION = '%s/media/public' % AWS_LOCATION
