6b38029640f844dfcfbd38ab796483dc45cc46f5
ceiphr <21004034+ceiphr@users.noreply.github.com>
2019-06-26 21:10:00 -0400

    Fix

--- ceiphr_proj/ceiphr/settings/production.py
+++ ceiphr_proj/ceiphr/settings/production.py
@@ -30,7 +30,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('SPACES_SECRET_ACCESS_KEY', 'spaces-secre
 AWS_STORAGE_BUCKET_NAME = os.environ.get('STORAGE_BUCKET_NAME', 'storage-bucket-name')
 AWS_S3_ENDPOINT_URL = 'https://sfo2.digitaloceanspaces.com'
 # AWS_S3_CUSTOM_DOMAIN = 'cdn.ceiphr.com'
-AWS_S3_CUSTOM_DOMAIN = 'cphr.sfo2.cdn.digitaloceanspaces.com/cphr'
+AWS_S3_CUSTOM_DOMAIN = 'cphr.sfo2.cdn.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=691200',
 }
