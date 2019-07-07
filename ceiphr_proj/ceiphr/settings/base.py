65029f499278d11af50b926384b8542535790864
ceiphr <21004034+ceiphr@users.noreply.github.com>
2019-07-07 16:07:54 -0400

    Tweaking CSP

--- ceiphr_proj/ceiphr/settings/base.py
+++ ceiphr_proj/ceiphr/settings/base.py
@@ -61,11 +61,11 @@ CSP_OBJECT_SRC = ("'none'",)
 
 CSP_BASE_URI = ("'none'",)
 
-CSP_WORKER_SRC = ("'none'",)
+CSP_WORKER_SRC = ("'self'",)
 
 CSP_FORM_ACTION = "'self'"
 
-CSP_DEFAULT_SRC = ("'none'", "https://disqus.com", "https://*.disquscdn.com",)
+CSP_DEFAULT_SRC = ("'self'", "https://disqus.com", "https://*.disquscdn.com",)
 
 
 # Application definition
